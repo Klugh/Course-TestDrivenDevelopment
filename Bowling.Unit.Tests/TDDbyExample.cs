aee37300f6b45a1e920952cb8d372006d5fd14e1
Douglas Klugh <dev@DouglasKlugh.com>
2019-03-25 22:58:16 -0700

    Fresh solution

---
+++ Bowling.Unit.Tests/TDDbyExample.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Xunit;
+
+namespace Bowling.Unit.Tests
+{
+    public class TDDbyExample
+    {
+    }
+}
---
+++ Bowling/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Bowling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
